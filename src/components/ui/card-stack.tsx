a7f963016260ed2d21560a1850d60567d0c9e7e1
Mayank Gupta <mayankkumargupta03@gmail.com>
2024-02-27 23:30:07 +0530

    slowness solved

--- src/components/ui/card-stack.tsx
+++ src/components/ui/card-stack.tsx
@@ -35,7 +35,7 @@ export const CardStack = ({
         newArray.unshift(newArray.pop()!); // move the last element to the front
         return newArray;
       });
-    }, 5000);
+    }, 8000);
 
     return () => clearInterval(interval);
   };
